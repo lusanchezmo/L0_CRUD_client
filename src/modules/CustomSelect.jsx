7d66eb59ff918d8bdcdcacda55261597c729eea8
luisda <lusanchezmo@unal.edu.co>
2024-04-11 21:40:06 -0500

    ?

--- src/modules/CustomSelect.jsx
+++ src/modules/CustomSelect.jsx
@@ -7,7 +7,7 @@ function CustomSelect(props) {
         <>
             <select onChange={(event) => props.handleInputChange(event, props.index)}/*value={selectedOption} onChange={handleSelectChange}*/>
                 <option value="">-- Selecciona --</option>
-                {props.item.getFields ? props.item.fieldsToSet.map((field, indexField) => (
+                {props.item.getFields ? props.item.fieldsToSet?.map((field, indexField) => (
                     <option key={indexField} value={props.item.funcionGetId ? props.item.funcionGetId(field) : props.item.funcionGetName(field)} >{props.item.funcionGetName(field)}</option>
                 )) : ""}
             </select>
